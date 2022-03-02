60b479547b15e4a589dd9b3cd04af81b4e7cfc29
Joshu Strassle <j.strassle@icloud.com>
2022-03-02 09:13:14 -0500

    merge control issues

--- src/App.tsx
+++ src/App.tsx
@@ -14,54 +14,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
 
-            <h1>This is a different header.</h1>
-
-            <ol className="center">
-                <li>Dogs</li>
-                <li>Everything Else</li>
-                <li>Cats</li>
-            </ol>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Joshua Strassle says Hello World.
-            </p>
-            <Button onClick={() => console.log("Hello World!")}>
-                {" "}
-                Log Hello World
-            </Button>
-            <Container>
-                <Row>
-                    <div
-                        style={{
-                            backgroundColor: "Red",
-                            width: "50%",
-                            height: "25%"
-                        }}
-                    >
-                        <Col>This looks horrible </Col>
-                    </div>
-                    <div
-                        style={{
-                            backgroundColor: "Red",
-                            width: "50%",
-                            height: "25%"
-                        }}
-                    >
-                        <Col>
-                            <img
-                                src="https://dogsbestlife.com/wp-content/uploads/2021/02/Weimaraner-puppies-scaled.jpeg"
-                                width={200}
-                                height={200}
-                                className={"float-right"}
-                                alt="Weim Puppies"
-                            />
-                        </Col>
-                    </div>
-                </Row>
-            </Container>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -74,7 +27,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
